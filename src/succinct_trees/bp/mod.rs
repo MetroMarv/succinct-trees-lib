35490b3c86ba370ab3e5bb48a59b89f0abe542c9
Cedrik Luedicke <luedic00@fiws200.cs.tu-dortmund.de>
2018-05-25 11:42:35 +0200

    added trait

--- src/succinct_trees/bp/mod.rs
+++ src/succinct_trees/bp/mod.rs
@@ -1,8 +1,66 @@
 use bit_vec::BitVec;
+use super::SuccinctTreeFunctions;
 
 pub struct BalancedParenthesis {
     parenthesis: BitVec
 }
+
+impl SuccinctTreeFunctions for BalancedParenthesis{
+
+    fn is_leaf(_lf:i32) -> bool{
+        unimplemented!();
+    }
+    fn first_child(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn next_sibling(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn parent(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn rank(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn select(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn close_rank(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn close_select(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn enclose(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn subtree_size(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn pre_rank(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn ancestor(_lf:i32, _lf2:i32) -> bool{
+        unimplemented!();
+    }
+    fn child(_lf:i32, _lf2:i32) -> i32{
+        unimplemented!();
+    }
+    fn lca(_lf:i32, _lf2:i32) -> i32{
+        unimplemented!();
+    }
+    fn level_ancestor(_lf:i32, _lf2:i32) -> i32{
+        unimplemented!();
+    }
+    fn degree(_lf:i32) -> i32{
+        unimplemented!();
+    }
+    fn depth(_lf:i32) -> i32{
+        unimplemented!();
+    }
+
+}
+
 
 impl BalancedParenthesis {
     pub fn new(parenthesis: BitVec) -> BalancedParenthesis {
--- src/succinct_trees/mod.rs
+++ src/succinct_trees/mod.rs
@@ -1,2 +1,22 @@
 pub mod bp;
 pub mod louds;
+
+pub trait SuccinctTreeFunctions{
+    fn is_leaf(i32) -> bool;
+    fn first_child(i32) -> i32;
+    fn next_sibling(i32) -> i32;
+    fn parent(i32) -> i32;
+    fn rank(i32) -> i32;
+    fn select(i32) -> i32;
+    fn close_rank(i32) -> i32;
+    fn close_select(i32) -> i32;
+    fn enclose(i32) -> i32;
+    fn subtree_size(i32) -> i32;
+    fn pre_rank(i32) -> i32;
+    fn ancestor(i32, i32) -> bool;
+    fn child(i32,i32) -> i32;
+    fn lca (i32, i32) -> i32;
+    fn level_ancestor(i32, i32) -> i32;
+    fn degree(i32) -> i32;
+    fn depth(i32) -> i32;
+}
