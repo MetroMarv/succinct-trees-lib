f15250bbf476737ab24ebcd463b2cd8afd8aa05a
cedlued <cedrikl.luedicke@tu-dortmund.de>
2018-06-01 10:13:42 +0200

    commit

--- src/succinct_trees/mod.rs
+++ src/succinct_trees/mod.rs
@@ -2,21 +2,21 @@ pub mod bp;
 pub mod louds;
 
 pub trait SuccinctTreeFunctions{
-    fn is_leaf(i32) -> bool;
-    fn first_child(i32) -> i32;
-    fn next_sibling(i32) -> i32;
-    fn parent(i32) -> i32;
-    fn rank(i32) -> i32;
-    fn select(i32) -> i32;
-    fn close_rank(i32) -> i32;
-    fn close_select(i32) -> i32;
-    fn enclose(i32) -> i32;
-    fn subtree_size(i32) -> i32;
-    fn pre_rank(i32) -> i32;
-    fn ancestor(i32, i32) -> bool;
-    fn child(i32,i32) -> i32;
-    fn lca (i32, i32) -> i32;
-    fn level_ancestor(i32, i32) -> i32;
-    fn degree(i32) -> i32;
-    fn depth(i32) -> i32;
+    fn is_leaf(_: i32) -> bool;
+    fn first_child(_: i32) -> i32;
+    fn next_sibling(_: i32) -> i32;
+    fn parent(_: i32) -> i32;
+    fn rank(_: i32) -> i32;
+    fn select(_: i32) -> i32;
+    fn close_rank(_: i32) -> i32;
+    fn close_select(_: i32) -> i32;
+    fn enclose(_: i32) -> i32;
+    fn subtree_size(_: i32) -> i32;
+    fn pre_rank(_: i32) -> i32;
+    fn ancestor(_: i32, _: i32) -> bool;
+    fn child(_: i32,_: i32) -> i32;
+    fn lca (_: i32, _: i32) -> i32;
+    fn level_ancestor(_: i32, _: i32) -> i32;
+    fn degree(_: i32) -> i32;
+    fn depth(_: i32) -> i32;
 }
